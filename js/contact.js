26386325645cc748e7e77fcdd8d961df782193eb
Harreson Lima <contatoharresonlima@gmail.com>
2024-09-07 21:54:01 -0300

    portfolio v1.2 done

--- js/contact.js
+++ js/contact.js
@@ -3,18 +3,41 @@ const contactBtn = document.querySelector(".contact__btn");
 contactBtn.addEventListener("click", () => {
   if(navigator.clipboard) {
     navigator.clipboard.writeText("contatoharresonlima@gmail.com");
-    Toastify({
-      text: "Contato copiado!",
-      duration: 3000,
-      close: true,
-      gravity: "bottom", 
-      position: "center", 
-      stopOnFocus: true, 
-      style: {
-        background: "#86c06c",
-        color: "#0c1336",
-        fontWeight: "bold",
-      },
-    }).showToast();
+
+
+
+    let url = window.location.pathname;
+    let filename = url.substring(url.lastIndexOf("/") + 1);
+    if (filename === "english-version.html") {
+      Toastify({
+        text: "Contact copied!",
+        duration: 3000,
+        close: true,
+        gravity: "bottom",
+        position: "center",
+        stopOnFocus: true,
+        style: {
+          background: "#86c06c",
+          color: "#0c1336",
+          fontWeight: "bold",
+        },
+      }).showToast();
+    } else {
+      Toastify({
+        text: "Contato copiado!",
+        duration: 3000,
+        close: true,
+        gravity: "bottom",
+        position: "center",
+        stopOnFocus: true,
+        style: {
+          background: "#86c06c",
+          color: "#0c1336",
+          fontWeight: "bold",
+        },
+      }).showToast();
+    }
+
+    
   }
 });
